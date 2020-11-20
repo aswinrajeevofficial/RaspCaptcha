85469e9dac86adc14204766e886348797e215742
aswinrajeevofficial <aswinworkofficial@gmail.com>
2020-11-20 12:24:01 +0000

    Modified script

--- start.sh
+++ start.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
+echo "Virtual environment activated"
+echo "Installing dependencies"
+pip3 install https://github.com/google-coral/pycoral/releases/download/release->
 pip install opencv-python
 
-DIR="/users/pgrad/rajeeva/aswin_venv/gitRepos/RaspCaptcha"
+DIR="/users/pgrad/rajeeva/aswin_venv/RaspCaptcha"
 if [ -d "$DIR" ]; then
   #Directory exists, so move into it and perform classification
   echo "Directory found, starting classification"
-  cd gitRepos/RaspCaptcha
-  ./classify_tflite.py --captcha-dir captchas_final --output stuff.txt --symbols symbols.txt
+  cd RaspCaptcha
+  ./classify_tflite.py --captcha-dir captchas_final --output stuff.txt --symbol>
 else
   #Directory doesn't exist, git clone and perform classification
   echo "Directory not found, cloning from git"
   git clone https://github.com/aswinrajeevofficial/RaspCaptcha.git
   cd RaspCaptcha
-  ./classify_tflite.py --captcha-dir captchas_final --output stuff.txt --symbols symbols.txt
+  ./classify_tflite.py --captcha-dir captchas_final --output stuff.txt --symbol>
   exit 1
 fi
